737344e79573f5d0f775c391082bca4a840580ff
rafaelborgesleal <rafaelborgesleal@hotmail.com>
2025-09-04 19:31:51 +0000

    Save

--- Chapter 2/Finding Duplicate Rows/Duplicate1.sql
+++ Chapter 2/Finding Duplicate Rows/Duplicate1.sql
@@ -1,8 +1,11 @@
+-- Select the customer information and how many orders they have
 SELECT Customer.CustomerID,
   Customer.FirstName,
   Customer.LastName,
+  -- Total number of orders for each customer
   COUNT(1) as NUM_ORDERS
 FROM Customer
+-- Start with the Customer table, Join with the Orders table, matching each customer to their orders
   JOIN Orders ON Customer.CustomerID = Orders.CustomerID
 GROUP BY Customer.CustomerID,
   Customer.FirstName,
