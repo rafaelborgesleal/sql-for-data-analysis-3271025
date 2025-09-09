245d3af06cf04a51a2c461a3c1d06d405804e2e5
rafaelborgesleal <rafaelborgesleal@hotmail.com>
2025-09-09 19:18:33 +0000

    Save

--- Chapter 2/Finding Inaccurate Data Values/CheckOrderTotal.sql
+++ Chapter 2/Finding Inaccurate Data Values/CheckOrderTotal.sql
@@ -1,8 +1,19 @@
-SELECT OrderItem.OrderId,
-  Orders.TotalDue,
-  SUM(Price * Quantity) as NewTotalDue
+-- Select order-level information and recalculate totals from items
+SELECT
+  OrderItem.OrderId,                   -- the ID of the order
+  Orders.TotalDue,                     -- the total amount stored in the Orders 
+  SUM(Price * Quantity) as NewTotalDue -- new total, calculated by summing each
+-- product's price X quantity
+-- Start from the OrderItem table (contains line items of each order)
 FROM OrderItem
-  JOIN Product ON OrderItem.ProductID = Product.ProductID
-  JOIN Orders ON OrderItem.OrderID = Orders.OrderID
-GROUP BY OrderItem.OrderId,
-  Orders.TotalDue
+-- Bring in product information, to get the Price for each item
+  JOIN Product
+    ON OrderItem.ProductID = Product.ProductID
+-- Bring in order-level information (e.g. stored TotalDue)
+  JOIN Orders
+    ON OrderItem.OrderID = Orders.OrderID
+-- Group results so that each order is represented once
+-- Required because we're using SUM() across multiple order items
+GROUP BY
+  OrderItem.OrderId,
+  Orders.TotalDue;
